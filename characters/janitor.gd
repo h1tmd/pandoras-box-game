6365230830587fea5f1c0715989411bbfeb6883a
h1tmd <105845103+h1tmd@users.noreply.github.com>
2024-04-17 00:07:06 +0800

    Added NPCs

---
+++ characters/janitor.gd
@@ -0,0 +1,26 @@
+extends AnimatedSprite2D
+
+var player_in = false
+#var interacted = false
+
+signal dialogue(title)
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Pandora":
+		player_in = true
+		material.set_shader_parameter("line_color", Color.WHITE)
+
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Pandora":
+		player_in = false
+		material.set_shader_parameter("line_color", Color.TRANSPARENT)
+
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("interact") and player_in:
+		#if not interacted:
+			dialogue.emit("janitor")
+			#interacted = true
+		#else:
+			#dialogue.emit("farmer2")
--- characters/pandora.gd
+++ characters/pandora.gd
@@ -5,7 +5,17 @@ const JUMP_VELOCITY = -1000.0
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
+var paused := false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func unpause(_res):
+	paused = false
+
+
+func _ready():
+	DialogueManager.dialogue_ended.connect(unpause)
+
 
 func _physics_process(delta):
 	# Animations
@@ -21,15 +31,23 @@ func _physics_process(delta):
 	# Gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Jump
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 	
 	# Horizontal Movement
 	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
+	if direction and not paused:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 20)
 	move_and_slide()
+
+
+func _unhandled_input(event):
+	# Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not paused:
+		velocity.y = JUMP_VELOCITY
+
+
+func prompt_dialogue(title):
+	var dialogue_file = load("res://dialogue/maindialogue.dialogue")
+	paused = true
+	DialogueManager.show_dialogue_balloon(dialogue_file, title)
