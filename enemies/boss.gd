304b637b19ce9bebd83243a282620a665a58a048
h1tmd <105845103+h1tmd@users.noreply.github.com>
2024-04-19 00:46:31 +0800

    Import enemies

---
+++ enemies/boss.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var pandora = get_parent().find_child("Pandora")
+
+var direction : Vector2
+@export var projectile_node : PackedScene
+
+func _process(delta):
+	direction = pandora.position - position
+	if direction.x > 0:
+		animated_sprite_2d.flip_h = true
+	else:
+		animated_sprite_2d.flip_h = false
+
+func _on_area_2d_body_entered(body):
+	if body == pandora:
+		print("hit pandora")
+
+func make_projectile(projectile_direction: Vector2):
+	var projectile = projectile_node.instantiate()
+	projectile.position = global_position
+	projectile.set_direction(projectile_direction)
+	get_tree().current_scene.call_deferred("add_child", projectile)
---
+++ enemies/zombie.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+const SPEED = 100
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var direction: Vector2 = Vector2.RIGHT * SPEED
+
+func _physics_process(delta):
+	enemy_gravity(delta)
+	enemy_wall_collision()
+	enemy_walk(delta)
+	enemy_flip(delta)
+
+	move_and_slide()
+
+func enemy_gravity(delta):
+	velocity.y += gravity * delta
+
+func enemy_walk(delta):
+	velocity.x = direction.x * SPEED * delta
+
+func enemy_wall_collision():
+	if is_on_wall():
+		direction.x *= -1
+
+func enemy_hit_edge():
+	if !is_on_floor():
+		direction.x *= -1
+
+func enemy_flip(delta):
+	if direction.x > 0:
+		get_node("AnimatedSprite2D").flip_h = false
+	else:
+		get_node("AnimatedSprite2D").flip_h = true
+		velocity.x = direction.x * SPEED * delta
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Pandora":
+		get_tree().change_scene_to_file("res://levels/level_1.tscn")
